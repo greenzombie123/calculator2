43ca16366114d4aabd72190084af8d28569b9148
Alvin Hart <105436283+greenzombie123@users.noreply.github.com>
2024-06-08 10:11:55 +0900

    Get input from user when they click a button

---
+++ script.js
@@ -0,0 +1,18 @@
+var input;
+var firstNumber = null;
+var secondNumber = null;
+var operator = null;
+var decimal = false;
+var buttons = document.querySelectorAll("button");
+buttons.forEach(function (button) {
+    var text = button.textContent;
+    if (text !== '=' && text !== 'backSpace' && text !== 'clear') {
+        button.addEventListener("click", getInput);
+    }
+});
+function getInput(event) {
+    input = event.target.textContent;
+    console.log(input);
+}
+function reset() { }
+function removeLastInput() { }
--- script.ts
+++ script.ts
@@ -1,3 +1,23 @@
-const a = 4.
+let input:number|string;
+let firstNumber:number|null = null;
+let secondNumber:number|null = null;
+let operator:string|null = null;
+let decimal = false;
 
-a
+
+const buttons = document.querySelectorAll("button")
+
+buttons.forEach(button=>{
+    const text = button.textContent!
+    if(text !== '=' && text !== 'backSpace' && text !== 'clear'){
+        button.addEventListener("click", getInput)
+    }
+})
+
+function getInput(event:Event){
+    input = (event.target as Element).textContent!
+    console.log(input)
+}
+
+function reset(){}
+function removeLastInput(){}
